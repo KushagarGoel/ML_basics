5043749533e621b9a5e3f4e7d9ff26c52c67305a
Kushagar Goel <54656743+KushagarGoel@users.noreply.github.com>
2020-08-23 16:40:49 +0530

    Add files via upload

---
+++ multiple linear regression/mlr.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 16:58:12 2020
+
+@author: kushagar
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv("50_Startups.csv")
+X = dataset.iloc[:,0:4].values
+y = dataset.iloc[:,4].values
+
+
+"""
+Encode the string values to 0,1,2...
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+LabelEncoder_X = LabelEncoder()
+X[: , 3] = LabelEncoder_X.fit_transform(X[:,3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+"""
+
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+LabelEncoder_X = LabelEncoder()
+X[: , 3] = LabelEncoder_X.fit_transform(X[:,3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+#Avoiding dummy variable trap
+X = X[:,1:]
+
+
+from sklearn.model_selection import train_test_split
+
+X_train,X_test , y_train, y_test = train_test_split(X,y,test_size = 0.2,random_state = 0)
+
+
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred1 = regressor.predict(X_test)
+
+import statsmodels.api as sm
+X = np.append(arr = np.ones((50,1)).astype(int), values = X , axis=1)#add array of 1 at index zero
+
+X_opt = X[:,[0,1,2,3,4,5]]
+regressor_ols = sm.OLS(endog=y,exog=X_opt).fit()
+regressor_ols.summary()
+
+
+X_opt = X[:,[0,1,3,4,5]]
+regressor_ols = sm.OLS(endog=y,exog=X_opt).fit()
+regressor_ols.summary()
+
+
+X_opt = X[:,[0,3]]
+regressor_ols = sm.OLS(endog=y,exog=X_opt).fit()
+regressor_ols.summary()
